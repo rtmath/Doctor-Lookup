21c9d322f734d7607d1c425fb98d3b1fa8d872ae
Ryan Mathisen <ryantmathisen@gmail.com>
2017-01-06 11:44:45 -0800

    FINALLY properly retrieve doctor informaton from api call, and add a tiny amount of styling to help visualize page format

--- content/js/lookup-interface.js
+++ content/js/lookup-interface.js
@@ -1,12 +1,38 @@
-var apiKey = require('./../.env').apiKey;
 var Practitioner = require('./../content/js/lookup.js').practModule;
 
 $('document').ready(function() {
-  console.log("jQuery initialized");
-  var doc = new Practitioner("Doc");
-  console.log(doc.name + " initialized");
   $('#userCond').submit(function(event) {
     event.preventDefault();
-    $('#searchResults').text("Condition submitted: " + $('#userCond > input').val());
+    var condition = $('#userCond > input').val();
+    $('#searchResults').text(condition);
+
+    var docs = [];
+    Practitioner.getAll(condition, docs);
+    setTimeout(function() {
+      // for (var i = 0; i < docs.length; i++) {
+      //   console.log(docs[i].profile.first_name + " " +
+      //               docs[i].profile.last_name);
+      // }
+      displayDoctors(docs);
+    }, 1000);
   });
 });
+
+var displayDoctors = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    $('#searchResults').append(
+      "<div class='profile'>" +
+      "<img src='" + array[i].profile.image_url + "'>" + " " + "<br>" +
+        "<span>" + array[i].profile.first_name + " " +
+                   array[i].profile.last_name + " " +
+                   array[i].profile.title + " " + "</span><hr>" +
+        "<p>" + array[i].practices[0].name + "<br>" +
+                array[i].practices[0].visit_address.street + " " +
+                array[i].practices[0].visit_address.city + ", " +
+                array[i].practices[0].visit_address.state + " " +
+                array[i].practices[0].visit_address.zip + "<br>" +
+                array[i].practices[0].phones[0].number + "</p>" +
+      "</div><br>"
+    );
+  }
+}
--- content/js/lookup.js
+++ content/js/lookup.js
@@ -1,5 +1,74 @@
-function Practitioner(name) {
-  this.name = name;
+var apiKey = require('./../../.env').apiKey;
+var getPractitioners = require('./../../.env').getDoctors;
+
+const MAXTODISPLAY = 5;
+
+function Practitioner() {
+  // this.fName = first;
+  // this.lName = last;
+  // this.pic = image;
+  // this.bio = bio;
+  // this.address = address;
+  // this.phone = phone;
+  // this.specialties = specialties;
+}
+
+Practitioner.getAll = function(condition, array) {
+  // console.log("getAll called");
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ condition+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+      for (var i = 0; i < MAXTODISPLAY; i++) {
+        // console.log("--Result Data: " + result.data[i]);
+        array[i] = result.data[i];
+        // console.log("--Array Data: " + array[i]);
+      }
+      return array;
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+
+Practitioner.prototype.test = function (condition) {
+  getPractitioners(condition);
+  setTimeout(function() {
+    for (var i = 0; i < 3; i++) {
+      console.log(i + ": " + results.data[i]);
+      console.log(i + ": " + results.data[i].profile.first_name);
+    }
+    console.log(practitioners[0]);
+  }, 2000);
+
 };
 
 exports.practModule = Practitioner;
+
+/*Object
+  Array[20]
+    i = Doctor Object
+      educations[]
+      group_affiliations[]
+      hospital_affiliations[]
+      insurances[]
+      npi
+      practices[]
+        distance
+        name
+        phones[]
+        visit_address
+        lat
+        lon
+      profile
+        bio
+        first_name
+        gender
+        image_url
+        languages[]
+        last_name
+        middle_name
+        slug
+        title
+      ratings[]
+      specialties[]
+*/
