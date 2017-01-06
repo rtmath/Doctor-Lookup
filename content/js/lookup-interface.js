7649cd32ffb9ae8ea65b1bc445291de187f885c0
Ryan Mathisen <ryantmathisen@gmail.com>
2017-01-06 12:36:16 -0800

    add format to the returned doctor results

--- content/js/lookup-interface.js
+++ content/js/lookup-interface.js
@@ -4,7 +4,7 @@ $('document').ready(function() {
   $('#userCond').submit(function(event) {
     event.preventDefault();
     var condition = $('#userCond > input').val();
-    $('#searchResults').text(condition);
+    // $('#searchResults').text(condition);
 
     var docs = [];
     Practitioner.getAll(condition, docs);
@@ -20,19 +20,34 @@ $('document').ready(function() {
 
 var displayDoctors = function(array) {
   for (var i = 0; i < array.length; i++) {
+    var profile = array[i].profile;
+    var practice = array[i].practices[0];
     $('#searchResults').append(
       "<div class='profile'>" +
-      "<img src='" + array[i].profile.image_url + "'>" + " " + "<br>" +
-        "<span>" + array[i].profile.first_name + " " +
-                   array[i].profile.last_name + " " +
-                   array[i].profile.title + " " + "</span><hr>" +
-        "<p>" + array[i].practices[0].name + "<br>" +
-                array[i].practices[0].visit_address.street + " " +
-                array[i].practices[0].visit_address.city + ", " +
-                array[i].practices[0].visit_address.state + " " +
-                array[i].practices[0].visit_address.zip + "<br>" +
-                array[i].practices[0].phones[0].number + "</p>" +
+        "<div class='left'>" +
+          "<img src='" + profile.image_url + "'>" + " " + "<br>" +
+            "<span>" + profile.first_name + " " +
+                       profile.last_name + ", " +
+                       profile.title + " " + "</span><hr>" +
+            "<p>" + practice.name + "<br>" +
+                    practice.visit_address.street + "<br>" +
+                    practice.visit_address.city + ", " +
+                    practice.visit_address.state + " " +
+                    practice.visit_address.zip + "<br>" +
+                    formatPhone(practice.phones[0].number) + "</p>" +
+        "</div>" +
+        "<div class='right'>" +
+          "<span>" + profile.bio + "</span>" +
+        "</div>" +
       "</div><br>"
     );
-  }
-}
+  };
+};
+
+var formatPhone = function(string) {
+  console.log("formatPhone called");
+  var areaCode = "(" + string[0] + string[1] + string[2] + ")";
+  var coCode = string[3] + string[4] + string[5];
+  var telCode = string[6] + string[7] + string[8] + string[9];
+  return areaCode + " " + coCode + "-" + telCode;
+};
